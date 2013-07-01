4cc0421d9a815597feed0476f0b4d294d2b02adf
tboeckmann <trb@bespecific.co.uk>
2013-07-01 17:45:03 +0100

    display results with hard coding working

--- lib/controllers/main.rb
+++ lib/controllers/main.rb
@@ -8,7 +8,6 @@ configure do
 end
 
 
-
   Amazon::Ecs.configure do |options|
     options[:response_group] = 'Large'
     options[:associate_tag] = ENV['ASSOCIATE_TAG']
@@ -16,18 +15,19 @@ end
     options[:AWS_secret_key] = ENV['AWS_SECRET_KEY']
   end
 
-get '/display_results' do
-  erb :results
-end
-
-
-get '/' do  
-  res = Amazon::Ecs.item_search('ruby', :country => 'uk')
-  res.items.each do |item|
-    # retrieve string value using XML path
-    puts item.get('ItemAttributes/Title')
+  get '/' do  
+    res = Amazon::Ecs.item_search('ruby', :country => 'uk')
+    res.items.each do |item|
+      # retrieve string value using XML path
+      puts item.get('ItemAttributes/Title')
+    end
+    erb :home
   end
-  erb :home
+
+  get '/display_results' do
+    erb :results
+  end
+
 
 end
 
