9a6848d4c8b0cf0d6ec3c9cbe2da3b152d34f151
dariodaprile <dario.daprile@gmail.com>
2013-07-01 18:13:30 +0100

    first commit

--- lib/controllers/main.rb
+++ lib/controllers/main.rb
@@ -1,14 +1,27 @@
 require 'sinatra/base'
+require 'amazon/ecs'
 
 class Main < Sinatra::Base
 
-configure do
-  set :root, Proc.new { File.join(File.dirname(__FILE__), "../../") }
-end
+  configure do
+    set :root, Proc.new { File.join(File.dirname(__FILE__), "../../") }
+  end
 
-get '/' do
-  erb :home
-end
+ Amazon::Ecs.configure do |options|
+    options[:response_group] = 'Large'
+    options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+    options[:AWS_secret_key] = ENV['AWS_SECRET_KEY']
+    options[:associate_tag] = ENV['ASSOCIATE_TAG']
+  end
+
+
+  get '/' do
+    erb :home
+  end
+
+  get '/display_results' do
+    erb :list
+  end
 
 
 end
